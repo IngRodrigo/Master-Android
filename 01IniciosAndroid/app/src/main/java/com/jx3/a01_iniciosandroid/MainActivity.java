5bee192ca3cd807d6a9b81b49ca96b2fafa73cb4
Rodrigo Jesus Sanchez <rodrigojesus-sanchez@aj.com.py>
2021-01-08 08:20:28 -0300

    commit initial

---
+++ 01IniciosAndroid/app/src/main/java/com/jx3/a01_iniciosandroid/MainActivity.java
@@ -0,0 +1,30 @@
+package com.jx3.a01_iniciosandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //ciclos de vida de un activity
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
